49a7fbbc0824b0467b1d947d6de22d01fe670d7d
chinhdoan <31790367+chinhdoan@users.noreply.github.com>
2020-05-05 17:00:43 +0700

    update image
    update image

---
+++ scripts.js
@@ -0,0 +1,24 @@
+
+var i = 0;
+var images = [];
+var time = 1200;
+
+// Image list
+images[0] = 'images/slider/1.jpg';
+images[1] = 'images/slider/2.jpg';
+images[2] = 'images/slider/3.jpg';
+
+
+function changeImage(){
+
+  document.slide.src = images[i];
+
+  if(i <images.length - 1){
+    i++;
+  } else {
+    i = 0;
+  }
+  setTimeout("changeImage()", time);
+}
+
+window.onload = changeImage;
